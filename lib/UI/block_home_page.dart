fe461b7dc6183e0b7edfb2e872eb0da2c74da867
Toan Do <toando0612@Toans-MacBook-Pro.local>
2019-07-01 15:14:34 +0700

    valid form, register, verified account

---
+++ lib/UI/block_home_page.dart
@@ -0,0 +1,54 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:giver_app/UI/customer_home_page.dart';
+import 'package:giver_app/UI/merchant_home_page.dart';
+import 'package:giver_app/UI/sign_in_page.dart';
+
+class BlockHomePage extends StatefulWidget {
+  @override
+  _BlockHomePageState createState() => _BlockHomePageState();
+}
+
+class _BlockHomePageState extends State<BlockHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text("You have to verify your account"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: signIn,
+                  child: Text('Sign In'),
+                ),
+                Container(width: 50.0,),
+                RaisedButton(
+                  onPressed: () {
+                    FirebaseUser user =
+                        FirebaseAuth.instance.currentUser() as FirebaseUser;
+                    user.sendEmailVerification();
+                    signIn();
+                  },
+                  child: Text("Don't receive? Send Again"),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> signIn() {
+    FirebaseAuth.instance.signOut();
+    Navigator.pushReplacement(
+        context, MaterialPageRoute(builder: (context) => SignInPage()));
+  }
+}
